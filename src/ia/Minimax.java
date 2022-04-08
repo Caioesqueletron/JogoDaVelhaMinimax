500c43bf098cd352ae393cc92ff2c0e184d2ae07
Caioesqueletron <caiocaio620@gmail.com>
2022-04-07 23:21:49 -0300

    Terminando MiniMax

--- src/ia/Minimax.java
+++ src/ia/Minimax.java
@@ -1,8 +1,49 @@
 package ia;
 
-public class Minimax {
+import jogo.Arvore;
+
+public class Minimax{
+
+	public int minimax(Arvore no, boolean isMax, int profundidade) {
+		int pontuacao = avaliarEstado();
+
+		if (Math.abs(pontuacao) == 1 || no.estaTudoPreenchido() || profundidade == 0) {
+			return pontuacao;
+		}
+		if (isMax) {
+			int maiorPontuacao = Integer.MIN_VALUE;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (no.estaMarcado(i, j)) {
+						no.marcarCampo(i, j, "X");
+						maiorPontuacao = Math.max(maiorPontuacao, minimax(no, false, profundidade - 1));
+						no.marcarCampo(i, j, "");
+					}
+				}
+			}
+
+			return maiorPontuacao;
+
+		} else {
+			int menorPontuacao = Integer.MAX_VALUE;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (no.estaMarcado(i, j)) {
+						no.marcarCampo(i, j, "O");
+						menorPontuacao = Math.min(menorPontuacao, minimax(no, true, profundidade - 1));
+						no.marcarCampo(i, j, "");
+
+					}
+				}
+			}
+			return menorPontuacao;
+
+		}
+
+	}
+
 	public int avaliarEstado() {
-		
+		return 0;
 	}
 
 }
--- src/jogo/Arvore.java
+++ src/jogo/Arvore.java
@@ -2,17 +2,34 @@ package jogo;
 
 public class Arvore {
 	boolean fimDeJogo, vez;
-	private int[][] no;
+	private String[][] no;
 	
 	public Arvore() {
-		this.no = new int[3][3];
+		this.no = new String[3][3];
 	}
 	
 	public void inicializar() {
 		for(int i = 0; i<3; i++) {
 			for(int j = 0; j<3;j++) {
-				
+				no[i][j] = " ";
 			}
 		}
+	}
+	
+	public boolean estaMarcado(int i, int j) {
+		if(this.no[i][j] == "X"|| this.no[i][j] == "O") {
+			return true;
+		}
+		
+		return false;
+	}
+
+	public void marcarCampo(int i, int j, String c) {
+		this.no[i][j] = c;
+	}
+
+	public boolean estaTudoPreenchido() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
